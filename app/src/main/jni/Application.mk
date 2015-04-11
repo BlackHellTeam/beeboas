a183471c6da8522326e557686fd35483d1a31a78
andforce <86118@163.com>
2015-04-11 17:24:54 +0800

    add libs

---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,5 @@
+#APP_ABI := armeabi armeabi-v7a
+#APP_ABI := x86
+#APP_ABI := armeabi armeabi-v7a arm64-v84 mips mips64 x86 x86_64
+APP_ABI := armeabi armeabi-v7a mips mips64 x86 x86_64
+APP_PLATFORM    := android-16 
